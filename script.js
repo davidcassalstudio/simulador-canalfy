065742d789f656ac792bcf0b3890c2677fff79ac
davidcassalstudio <davidcassal@gmail.com>
2021-04-06 17:41:07 -0300

    Formatação
    Ajustes na formatação dos números

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ function calc() {
     document.getElementById("clientName").innerHTML = clientName;
 
     // Calcula o valor de receita (Ticket Médio x Qtd. de Parceiros x Taxa de Conversão em Porcentagem)
-    var revenue = parseFloat(ticket) * parseFloat(partners) * parseFloat(conversionPercent)/100;
+    var revenuePartial = parseFloat(ticket) * parseFloat(partners) * parseFloat(conversionPercent)/100;
+    var revenue = revenuePartial.toFixed(2);
     document.getElementById("revenue").innerHTML = revenue;
 
     // Calcula o valor da comissão de parceiros (Receita x Comissão Percentual)
-    var comission = parseFloat(revenue) * parseFloat(comissionPercent)/100;
+    var comissionPartial = parseFloat(revenue) * parseFloat(comissionPercent)/100;
+    var comission = comissionPartial.toFixed(2);
     document.getElementById("comission").innerHTML = comission;
 
     // Calcula o valor do número de pedidos (Receita / Ticket Médio)
@@ -27,7 +29,8 @@ function calc() {
     document.getElementById("partnersNumber").innerHTML = partnersNumber;
 
     // Exibe o ticket médio
-    var ticketMiddle = parseFloat(ticket);
+    var ticketPartial = parseFloat(ticket);
+    var ticketMiddle = ticketPartial.toFixed(2);
     document.getElementById("ticketMiddle").innerHTML = ticketMiddle;
 
     // Exibe a taxa de conversão
@@ -39,14 +42,17 @@ function calc() {
     document.getElementById("comissionRate").innerHTML = comissionRate;
 
     // Exibe a mensalidade da Canalfy
-    var canalfy = parseFloat(monthlyCanalfy);
+    var canalfyPartial = parseFloat(monthlyCanalfy);
+    var canalfy = canalfyPartial.toFixed(2);
     document.getElementById("canalfy").innerHTML = canalfy;
 
     // Calcula o valor do total investido (Comissão dos Parceiros + Mensalidade da Canalfy)
-    var totalInvested = parseFloat(comission) + parseFloat(monthlyCanalfy);
+    var investedPartial = parseFloat(comission) + parseFloat(monthlyCanalfy);
+    var totalInvested = investedPartial.toFixed(2);
     document.getElementById("totalInvested").innerHTML = totalInvested;
 
+    // Calcula o valor percentual do custo de venda (Total Investido / Receita)
     var costPartial = parseFloat(totalInvested) / parseFloat(revenue);
     var costOfSale = parseFloat(costPartial)*100;
-    document.getElementById("costOfSale").innerHTML = costOfSale;
+    document.getElementById("costOfSale").innerHTML = Math.round(costOfSale);
 }
